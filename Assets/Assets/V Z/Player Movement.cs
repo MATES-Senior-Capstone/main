d6fab164926ca6608289be6b05bf20e1072fff36
traviswadeeatseggs <jennifer.kim@ocvts.org>
2025-04-17 04:43:31 -0400

    player walking animation
    fixed movement script to apply animations in game

--- Assets/Assets/V Z/Player Movement.cs
+++ Assets/Assets/V Z/Player Movement.cs
@@ -9,13 +9,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
    public Animator animator;
 
-   Vector2 movement;
+   public Vector2 movement;
 
    public float runSpeed = 20.0f;
 
    void Start()
    {
     body = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
    }
 
    void Update ()
@@ -23,6 +24,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     //fluxes between -1 and 1
     horizontal = Input.GetAxisRaw("Horizontal"); // -1 is to left
     vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+    movement = new Vector2(horizontal, vertical);
 
     animator.SetFloat("horizontal", movement.x);
     animator.SetFloat("vertical", movement.y);
